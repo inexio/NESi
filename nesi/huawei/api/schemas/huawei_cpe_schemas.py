0477090a23d3fbeffa2c3d5cdc53cd29d92be556
unkn0wn-user <janis.gross.jg@gmail.com>
2020-09-03 11:13:35 +0200

    Added copyright disclaimer to various files

--- nesi/huawei/api/schemas/huawei_cpe_schemas.py
+++ nesi/huawei/api/schemas/huawei_cpe_schemas.py
@@ -1,3 +1,15 @@
+# This file is part of the NESi software.
+#
+# Copyright (c) 2020
+# Original Software Design by Ilya Etingof <https://github.com/etingof>.
+#
+# Software adapted by inexio <https://github.com/inexio>.
+# - Janis Groß <https://github.com/unkn0wn-user>
+# - Philip Konrath <https://github.com/Connyko65>
+# - Alexander Dincher <https://github.com/Dinker1996>
+#
+# License: https://github.com/inexio/NESi/LICENSE.rst
+
 from nesi.softbox.api.schemas.cpe_schemas import *
 
 
