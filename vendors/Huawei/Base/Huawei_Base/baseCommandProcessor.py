9c7abb0e2d2fbabb3f8e6f34ee47af70ade4cc75
unkn0wn-user <janis.gross.jg@gmail.com>
2020-09-08 11:31:02 +0200

    Fixed wrong mapping for service_port states

--- vendors/Huawei/Base/Huawei_Base/baseCommandProcessor.py
+++ vendors/Huawei/Base/Huawei_Base/baseCommandProcessor.py
@@ -25,15 +25,19 @@ class BaseCommandProcessor(base.CommandProcessor):
         if object.admin_state == '0':
             if type == 'subrack':
                 object.admin_state = 'lock'
-            elif type in ('card', 'port', 'ont_port', 'service_port'):
+            elif type in ('card', 'port', 'ont_port'):
                 object.admin_state = 'deactivated'
+            elif type == 'service_port':
+                object.admin_state = 'disable'
             elif type in ('ont', 'ont_port', 'cpe'):
                 object.admin_state = 'offline'
         elif object.admin_state == '1':
             if type == 'subrack':
                 object.admin_state = 'unlock'
-            elif type in ('card', 'port', 'ont_port', 'service_port'):
+            elif type in ('card', 'port', 'ont_port'):
                 object.admin_state = 'activated'
+            elif type == 'service_port':
+                object.admin_state = 'enable'
             elif type in ('ont', 'ont_port', 'cpe'):
                 object.admin_state = 'online'
         elif object.admin_state == '2':
@@ -43,15 +47,19 @@ class BaseCommandProcessor(base.CommandProcessor):
         if object.operational_state == '0':
             if type in ('subrack', 'card'):
                 object.operational_state = 'disabled'
-            elif type in ('port', 'service_port'):
+            elif type == 'port':
                 object.operational_state = 'deactivated'
+            elif type == 'service_port':
+                object.operational_state = 'down'
             elif type in ('ont', 'ont_port'):
                 object.operational_state = 'offline'
         elif object.operational_state == '1':
             if type in ('subrack', 'card'):
                 object.operational_state = 'enabled'
-            elif type in ('port', 'service_port'):
+            elif type == 'port':
                 object.operational_state = 'activated'
+            elif type == 'service_port':
+                object.operational_state = 'up'
             elif type in ('ont', 'ont_port'):
                 object.operational_state = 'online'
         elif object.operational_state == '2':
