c55b8cef8e9e5c11bfb22922bb11416b84813bbc
unkn0wn-user <janis.gross.jg@gmail.com>
2020-09-08 11:49:19 +0200

    Added further components to huawei setup script

--- bootup/conf/bootstraps/create-huawei-5623.sh
+++ bootup/conf/bootstraps/create-huawei-5623.sh
@@ -21,51 +21,51 @@ path="`dirname \"$0\"`"
 
 #--------------------------------------------------------#
 #                                                        #
-#   Subrack 1/1                                          #
-#   |---> Card 1/1/1 (vdsl)                              #
-#   |     |-> Port 1/1/1/1                               #
-#   |     |   |-> Cpe 1/1/1/1/1                          #
-#   |     |       |-> CpePort 1/1/1/1/1/1                #
-#   |     |-> Port 1/1/1/2                               #
-#   |     |   |-> Cpe 1/1/1/2/1                          #
-#   |     |       |-> CpePort 1/1/1/2/1/1                #
-#   |     |-> Port 1/1/1/3                               #
+#   Subrack 0                                            #
+#   |---> Card 0/0   (vdsl)                              #
+#   |     |-> Port 0/0/0                                 #
+#   |     |   |-> Cpe 0/0/0 1                            #
+#   |     |       |-> CpePort 0/0/0 1/1                  #
+#   |     |-> Port 0/0/1                                 #
+#   |     |   |-> Cpe 0/0/1 1                            #
+#   |     |       |-> CpePort 0/0/1 1/1                  #
+#   |     |-> Port 0/0/2                                 #
 #   |                                                    #
-#   |---> Card 1/1/2 (adsl)                              #
-#   |     |-> Port 1/1/2/1                               #
-#   |     |   |-> Cpe 1/1/2/1/1                          #
-#   |     |       |-> CpePort 1/1/2/1/1/1                #
-#   |     |-> Port 1/1/2/2                               #
-#   |     |   |-> Cpe 1/1/2/2/1                          #
-#   |     |       |-> CpePort 1/1/2/2/1/1                #
-#   |     |-> Port 1/1/2/3                               #
+#   |---> Card 0/1   (adsl)                              #
+#   |     |-> Port 0/1/0                                 #
+#   |     |   |-> Cpe 0/1/0 1                            #
+#   |     |       |-> CpePort 0/1/0 1/1                  #
+#   |     |-> Port 0/1/1                                 #
+#   |     |   |-> Cpe 0/1/1 1                            #
+#   |     |       |-> CpePort 0/1/1 1/1                  #
+#   |     |-> Port 0/1/2                                 #
 #   |                                                    #
-#   |---> Card 1/1/4 (ftth)                              #
-#   |     |-> Port 1/1/4/1                               #
-#   |     |   |-> Ont 1/1/4/1/1                          #
-#   |     |       |-> OntPort 1/1/4/1/1/1/1              #
-#   |     |           |-> Cpe 1/1/4/1/1/1/1/1            #
-#   |     |               |-> CpePort 1/1/4/1/1/1/1/1/1  #
-#   |     |-> Port 1/1/4/2                               #
-#   |     |   |-> Ont 1/1/4/2/1                          #
-#   |     |       |-> OntPort 1/1/4/2/1/1/1              #
-#   |     |           |-> Cpe 1/1/4/2/1/1/1/1            #
-#   |     |               |-> CpePort 1/1/4/2/1/1/1/1/1  #
-#   |     |-> Port 1/1/4/3                               #
+#   |---> Card 0/2   (ftth)                              #
+#   |     |-> Port 0/2/0                                 #
+#   |     |   |-> Ont                           #
+#   |     |       |-> OntPort               #
+#   |     |           |-> Cpe             #
+#   |     |               |-> CpePort   #
+#   |     |-> Port 0/2/1                                 #
+#   |     |   |-> Ont                           #
+#   |     |       |-> OntPort               #
+#   |     |           |-> Cpe             #
+#   |     |               |-> CpePort   #
+#   |     |-> Port 0/2/2                                 #
 #   |                                                    #
-#   |---> Card 1/1/5 (ftth-pon)                          #
-#         |-> Port 1/1/5/1                               #
-#             |-> Ont 1/1/5/1/1                          #
-#             |   |-> OntPort 1/1/5/1/1/1/1              #
-#             |       |-> Cpe 1/1/5/1/1/1/1/1            #
-#             |           |-> CpePort 1/1/5/1/1/1/1/1/1  #
-#             |-> Ont 1/1/5/1/2                          #
-#                 |-> OntPort 1/1/5/1/2/1/1              #
-#                 |   |-> Cpe 1/1/5/1/2/1/1/1            #
-#                 |       |-> CpePort 1/1/5/1/2/1/1/1/1  #
-#                 |-> OntPort 1/1/5/1/2/1/2              #
-#                     |-> Cpe 1/1/5/1/2/1/2/1            #
-#                         |-> CpePort 1/1/5/1/2/1/2/1/1  #
+#   |---> Card 0/3   (ftth-pon)                          #
+#         |-> Port 0/3/0                                 #
+#             |-> Ont                           #
+#             |   |-> OntPort               #
+#             |       |-> Cpe             #
+#             |           |-> CpePort   #
+#             |-> Ont                           #
+#                 |-> OntPort               #
+#                 |   |-> Cpe             #
+#                 |       |-> CpePort   #
+#                 |-> OntPort               #
+#                     |-> Cpe             #
+#                         |-> CpePort   #
 #                                                        #
 #--------------------------------------------------------#
 
@@ -147,6 +147,28 @@ req='{
 
 port_0_0_0=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
 
+### Cpe at port 0/0/0 ###
+
+# Create a physical cpe at the ont-port (admin operation)
+req='{
+  "port_id": '$port_0_0_0',
+  "description": "Cpe 0/0/0 1",
+  "mac": "03:ed:5d:a1:4d:5d",
+  "admin_state": "1"
+}'
+
+cpe_0_0_0_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpes)
+
+### CpePort 1 ###
+
+# Create a physical cpe-port at the cpe (admin operation)
+req='{
+  "cpe_id": '$cpe_0_0_0_1',
+  "description": "CpePort 0/0/0 1/1"
+}'
+
+cpe_port_0_0_0_1_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpe_ports)
+
 ### PORT 0/0/1 and deps ###
 
 # Create a physical port at the network device (admin operation)
@@ -164,6 +186,28 @@ req='{
 
 port_0_0_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
 
+### Cpe at port 0/0/1 ###
+
+# Create a physical cpe at the ont-port (admin operation)
+req='{
+  "port_id": '$port_0_0_1',
+  "description": "Cpe 0/0/1 1",
+  "mac": "8e:1c:02:05:a3:dc",
+  "admin_state": "0"
+}'
+
+cpe_0_0_1_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpes)
+
+### CpePort 1 ###
+
+# Create a physical cpe-port at the cpe (admin operation)
+req='{
+  "cpe_id": '$cpe_0_0_1_1',
+  "description": "CpePort 0/0/1 1/1"
+}'
+
+cpe_port_0_0_1_1_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpe_ports)
+
 ### PORT 0/0/2 and deps ###
 
 # Create a physical port at the network device (admin operation)
@@ -179,7 +223,7 @@ req='{
   "operational_state": "0"
 }'
 
-port_0_0_3=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
+port_0_0_2=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
 
 ### Card 0/1 ###
 
@@ -210,6 +254,28 @@ req='{
 
 port_0_1_0=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
 
+### Cpe at port 0/1/0 ###
+
+# Create a physical cpe at the ont-port (admin operation)
+req='{
+  "port_id": '$port_0_1_0',
+  "description": "Cpe 0/1/0 1",
+  "mac": "61:26:5c:eb:8a:a6",
+  "admin_state": "1"
+}'
+
+cpe_0_1_0_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpes)
+
+### CpePort 1 ###
+
+# Create a physical cpe-port at the cpe (admin operation)
+req='{
+  "cpe_id": '$cpe_0_1_0_1',
+  "description": "CpePort 0/1/0 1/1"
+}'
+
+cpe_port_0_1_0_1_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpe_ports)
+
 ### PORT 0/1/1 and deps ###
 
 # Create a physical port at the network device (admin operation)
@@ -226,6 +292,28 @@ req='{
 }'
 
 port_0_1_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
+
+### Cpe at port 0/1/1 ###
+
+# Create a physical cpe at the ont-port (admin operation)
+req='{
+  "port_id": '$port_0_1_1',
+  "description": "Cpe 0/1/1 1",
+  "mac": "49:a6:23:91:f4:7b",
+  "admin_state": "0"
+}'
+
+cpe_0_1_1_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpes)
+
+### CpePort 1 ###
+
+# Create a physical cpe-port at the cpe (admin operation)
+req='{
+  "cpe_id": '$cpe_0_1_1_1',
+  "description": "CpePort 0/1/1 1/1"
+}'
+
+cpe_port_0_1_1_1_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cpe_ports)
 
 ### PORT 0/1/2 and deps ###
 
@@ -307,3 +395,65 @@ req='{
 
 port_0_1_2=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
 
+### Card 0/3 ###
+
+# Create a physical card at the network device (admin operation)
+req='{
+  "subrack_id": '$subrack_0',
+  "description": "Physical card 0/3",
+  "product": "ftth-pon",
+  "board_name": "H807GPBH"
+}'
+
+card_0_3=$(create_resource "$req" $ENDPOINT/boxen/$box_id/cards)
+
+### PORT 0/3/0 and deps ###
+
+# Create a physical port at the network device (admin operation)
+req='{
+  "card_id": '$card_0_3',
+  "description": "Physical port 0/3/0",
+  "loopback": "disable",
+  "upstream": 10000,
+  "downstream": 25000,
+  "upstream_max": 100000,
+  "downstream_max": 100000,
+  "admin_state": "1",
+  "operational_state": "1"
+}'
+
+port_0_3_0=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
+
+### PORT 0/3/1 and deps ###
+
+# Create a physical port at the network device (admin operation)
+req='{
+  "card_id": '$card_0_2',
+  "description": "Physical port 0/3/1",
+  "loopback": "disable",
+  "upstream": 0,
+  "downstream": 0,
+  "upstream_max": 100000,
+  "downstream_max": 100000,
+  "admin_state": "1",
+  "operational_state": "0"
+}'
+
+port_0_3_1=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
+
+### PORT 0/3/2 and deps ###
+
+# Create a physical port at the network device (admin operation)
+req='{
+  "card_id": '$card_0_2',
+  "description": "Physical port 0/3/2",
+  "loopback": "disable",
+  "upstream": 0,
+  "downstream": 0,
+  "upstream_max": 100000,
+  "downstream_max": 100000,
+  "admin_state": "0",
+  "operational_state": "0"
+}'
+
+port_0_3_2=$(create_resource "$req" $ENDPOINT/boxen/$box_id/ports)
