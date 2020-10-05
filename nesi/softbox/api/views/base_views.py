f5fb2cbbaabaa4b420c524b30cf46c4a42b52c32
unkn0wn-user <janis.gross.jg@gmail.com>
2020-10-05 12:29:31 +0200

    Fixed base_views to handle new keymile schemas

--- nesi/softbox/api/views/base_views.py
+++ nesi/softbox/api/views/base_views.py
@@ -20,6 +20,7 @@ from nesi.softbox.api.models.box_models import Box
 from nesi.softbox.api.schemas import *
 from nesi.alcatel.api.schemas import *
 from nesi.huawei.api.schemas import *
+from nesi.keymile.api.schemas import *
 # important for other view classes
 from nesi.softbox.api import db
 
