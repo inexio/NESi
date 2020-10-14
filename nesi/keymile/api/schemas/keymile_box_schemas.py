e3173f8b5ddb035caff0253d95ac58eeb6700afe
png2000 <p.gross@deutsche-glasfaser.de>
2020-10-14 09:15:32 +0200

    create-keymile-MG2500: Name hinzugefügt currTemperature hinzugefügt

--- nesi/keymile/api/schemas/keymile_box_schemas.py
+++ nesi/keymile/api/schemas/keymile_box_schemas.py
@@ -7,16 +7,17 @@
 # - Janis Groß <https://github.com/unkn0wn-user>
 # - Philip Konrath <https://github.com/Connyko65>
 # - Alexander Dincher <https://github.com/Dinker1996>
+# - Philipp-Noah Groß <https://github.com/pngross>
 #
 # License: https://github.com/inexio/NESi/LICENSE.rst
 
 from nesi.softbox.api.schemas.box_schemas import *
 
 
-class HuaweiBoxSchema(BoxSchema):
+class KeymileBoxSchema(BoxSchema):
     class Meta:
         model = Box
-        fields = BoxSchema.Meta.fields + ('channels', 'interfaces')
+        fields = BoxSchema.Meta.fields + ('channels', 'interfaces', 'currTemperature')
 
     interfaces = ma.Hyperlinks(
         {'_links': {
--- nesi/softbox/api/models/box_models.py
+++ nesi/softbox/api/models/box_models.py
@@ -7,6 +7,7 @@
 # - Janis Groß <https://github.com/unkn0wn-user>
 # - Philip Konrath <https://github.com/Connyko65>
 # - Alexander Dincher <https://github.com/Dinker1996>
+# - Philipp-Noah Groß <https://github.com/pngross>
 #
 # License: https://github.com/inexio/NESi/LICENSE.rst
 import uuid
@@ -54,10 +55,13 @@ class Box(db.Model):
     credentials = db.relationship('Credential', backref='Box', lazy='dynamic')
     credential_details = db.relationship('Credential', backref='credentials', lazy='dynamic')
     users = db.relationship('User', backref='Box', lazy='dynamic')
+
     subracks = db.relationship('Subrack', backref='Box', lazy='dynamic')
     subrack_details = db.relationship('Subrack', backref='subracks', lazy='dynamic')
     cards = db.relationship('Card', backref='Box', lazy='dynamic')
+    mgmt_cards = db.relationship('MgmtCard', backref='Box', lazy='dynamic')
     ports = db.relationship('Port', backref='Box', lazy='dynamic')
+    mgmt_ports = db.relationship('MgmtPort', backref='Box', lazy='dynamic')
     channels = db.relationship('Channel', backref='Box', lazy='dynamic')
     interfaces = db.relationship('Interface', backref='Box', lazy='dynamic')
     cpes = db.relationship('Cpe', backref='Box', lazy='dynamic')
@@ -114,3 +118,4 @@ class Box(db.Model):
     pitp_mode = db.Column(db.String(), default='')
     dsl_mode = db.Column(db.Enum('tr165', 'tr129'), default='tr165')
 
+    currTemperature = db.Column(db.Integer(), default=15)
