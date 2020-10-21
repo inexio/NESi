1f20e676b48e9a1cb82f1c3ae2a36b313100de0a
Connyko65 <ash-pikachu@t-online.de>
2020-10-21 09:20:05 +0200

    Corrected Mgmt Card/Mgmt Port management functions

--- vendors/KeyMile/accessPoints/root/mgmt_unit/mgmt_port/mgmtportManagementFunctions.py
+++ vendors/KeyMile/accessPoints/root/mgmt_unit/mgmt_port/mgmtportManagementFunctions.py
@@ -1,334 +1,216 @@
 main = {
-    'General': {
-        'Prop': {
-            'Labels': 'rw',
-            'AlarmStatus': 'r-'
-        }
-    },
-    'AdminAndOperStatus': {
-        'Prop': {
-            'AdministrativeStatus': 'rw',
-            'OperationalStatus': 'r-'
+        'General': {
+            'Prop': {
+                'Labels': 'rw',
+                'AlarmStatus': 'r-'
+            }
+        },
+        'AdminAndOperStatus': {
+            'Prop': {
+                'AdministrativeStatus': 'rw',
+                'OperationalStatus': 'r-'
+            }
+        },
+        'SFP': {
+            'Prop': {
+                'EquipmentInventory': 'r-'
+            }
         }
     }
-}
 
 cfgm = {
-    'Multicast': {
-        'Prop': {
-            'MaxNumberOfMulticastStreams': 'rw',
-            'EnableIgmpClassifier': 'rw',
-            'AllowStaticStreams': 'rw',
-            'EnableFastLeave': 'rw',
-            'GroupManagement': 'rw',
-            'Bandwidth': 'rw'
+        'PortMode': {
+            'Prop': {
+                'PortMode': 'rw',
+                'Redundancy': 'rw',
+                'PortVlan': 'rw',
+                'MTUSize': 'rw'
+            }
         },
-        'Cmd': (
-            'GetGroupList',
-        )
-    },
-    'Traceability': {
-        'Prop': {
-            'AgentRemoteId': 'rw'
-        }
-    },
-    'Security': {
-        'Prop': {
-            'ServiceOptions': 'rw',
-            'MaxNumberOfMac': 'rw'
-        }
-    },
-    'AccessControl': {
-        'Prop': {
-            'ClassificationKey': 'rw',
-            'MAT': 'rw'
-        }
-    },
-    'RateLimiter': {
-        'Prop': {
-            'RateLimiting': 'rw',
-            'RateLimitingCoS': 'rw'
-        }
-    },
-    'Qos': {
-        'Prop': {
-            'WfqProfile': 'rw'
-        }
-    },
-    'Wire': {
-        'Prop': {
-            'MeltConfiguration': 'rw'
-        }
-    },
-    'Profiles': {
-        'Prop': {
-            'PortProfiles': 'rw'
-        }
-    },
-    'Misc': {
-        'Prop': {
-            'SpecificDPBO': 'rw',
-            'SpecificUPBO': 'rw'
+        'Rstp': {
+            'Prop': {
+                'Rstp': 'rw',
+                'RstpParams': 'rw'
+            }
+        },
+        'VlanList': {
+            'Prop': {
+                'ManagementVlan': 'r-',
+                'ListType': 'rw',
+                'VlanList': 'rw'
+            },
+            'Cmd': (
+                'FlushVlanList',
+            )
+        },
+        'QoS': {
+            'Prop': {
+                'SchedulingProfileName': 'rw'
+            }
+        },
+        'Multicast': {
+            'Prop': {
+                'EnableIgmpClassifier': 'rw',
+                'AllowStaticStreams': 'rw',
+                'MulticastPortMode': 'rw'
+            }
+        },
+        'LinkAggregation': {
+            'Prop': {
+                'LinkAggregation': 'rw'
+            }
+        },
+        'PHY': {
+            'Prop': {
+                'PhyMode': 'rw',
+                'PhyFlowControl': 'rw'
+            }
+        },
+        'Eoam': {
+            'Prop': {
+                'NetworkNetworkInterface': 'rw'
+            }
+        },
+        'PriorityMapping': {
+            'Prop': {
+                'DSCPTo802Dot1qTxPriorityMapping': 'rw'
+            }
+        },
+        'Mirroring': {
+            'Prop': {
+                'MirrorPortList': 'rw'
+            }
         }
     }
-}
 
 fm = {
-    'Status': {
-        'Prop': {
-            'AlarmStatus': 'r-'
+        'Status': {
+            'Prop': {
+                'AlarmStatus': 'r-'
+            },
+            'Cmd': (
+                'Acknowledge',
+            )
         },
-        'Cmd': (
-            'Acknowledge',
-        )
-    },
-    'Configuration': {
-        'Prop': {
-            'AlarmConfiguration': 'rw'
+        'Configuration': {
+            'Prop': {
+                'AlarmConfiguration': 'rw'
+            }
         }
     }
-}
 
 pm = {
-    'PerformanceMonitoring': {
-        'Cmd': (
-            'UserCounter',
-            'GetHistory15min',
-            'GetHistory24h',
-            'GetAlarm15min',
-            'GetAlarm24hRecursive',
-            'ResetUserCounter',
-            'ResetAlarm15min',
-            'ResetAlarm24h'
-        )
-    },
-    'UserCounter': {
-        'Prop': {
-            'UserCounterDisplayMode': 'rw',
-            'UserCounterTable': 'r-'
+        'UserCounter': {
+            'Prop': {
+                'UserCounterDisplayMode': 'rw',
+                'UserCounterTable': 'r-'
+            },
+            'Cmd': (
+                'UserCounterReset',
+            )
         },
-        'Cmd': (
-            'UserCounterReset',
-        )
-    },
-    'History15min': {
-        'Prop': {
-            'History15minDisplayMode': 'rw',
-            'History15minTable': 'r-'
+        'History15min': {
+            'Prop': {
+                'History15minDisplayMode': 'rw',
+                'History15minTable': 'r-'
+            }
+        },
+        'History24h': {
+            'Prop': {
+                'History24hDisplayMode': 'rw',
+                'History24hTable': 'r-'
+            }
+        },
+        'Alarm15min': {
+            'Prop': {
+                'Alarm15minDisplayMode': 'rw',
+                'Alarm15minTable': 'r-'
+            },
+            'Cmd': (
+                'Alarm15minReset',
+            )
+        },
+        'Alarm24h': {
+            'Prop': {
+                'Alarm24hDisplayMode': 'rw',
+                'Alarm24hTable': 'r-'
+            },
+            'Cmd': (
+                'Alarm24hReset',
+            )
+        },
+        'PerformanceMonitoring': {
+            'Cmd': (
+                'UserCounter',
+                'GetHistory15min',
+                'GetHistory24h',
+                'GetAlarm15min',
+                'GetAlarm24hRecursive',
+                'ResetUserCounter',
+                'ResetAlarm15min',
+                'ResetAlarm24h'
+            )
         }
-    },
-    'History24h': {
-        'Prop': {
-            'History24hDisplayMode': 'rw',
-            'History24hTable': 'r-'
-        }
-    },
-    'Alarm15min': {
-        'Prop': {
-            'Alarm15minDisplayMode': 'rw',
-            'Alarm15minTable': 'r-'
-        },
-        'Cmd': (
-            'Alarm15minReset',
-        )
-    },
-    'Alarm24h': {
-        'Prop': {
-            'Alarm24hDisplayMode': 'rw',
-            'Alarm24hTable': 'r-'
-        },
-        'Cmd': (
-            'Alarm24hReset',
-        )
     }
-}
 
 status = {
-    'General': {
-        'Prop': {
-            'Standard': 'r-',
-            'PowerMgmStatus': 'r-',
-            'Vdsl2Parameters': 'r-',
-            'EstUPBOElectricalLength': 'r-',
-            'LineRate': 'r-',
-            'LineSnrMargin': 'r-',
-            'AttainableNetDataRate': 'r-',
-            'AttainableRate': 'r-',
-            'OutputPower': 'r-',
-            'BandStatus': 'r-'
-        }
-    },
-    'statistics': {
-        'Prop': {
-            'counters': 'r-',
-            'PolicingCounters': 'r-'
-        },
-        'Cmd': (
-            'ResetPortCounters',
-        )
-    },
-    'Nto1MacAccessDynamicList': {
-        'Prop': {
-            'UnicastList': 'r-'
-        }
-    },
-    'HostPortStatistics': {
-        'GeneralCounters': {
+        'PortMode': {
             'Prop': {
-                'GeneralList': 'r-'
+                'DefaultConfigEnabled': 'r-'
+            }
+        },
+        'MAC': {
+            'Prop': {
+                'PortMacStatus': 'r-'
+            }
+        },
+        'Phy': {
+            'Prop': {
+                'PortLinkStatus': 'r-'
+            }
+        },
+        'Rstp': {
+            'Prop': {
+                'PortRstpStatus': 'r-'
+            }
+        },
+        'LinkAggregation': {
+            'Prop': {
+                'Status': 'r-'
+            }
+        },
+        'Ddm': {
+            'Prop': {
+                'DdmStatus': 'r-'
+            }
+        },
+        'QoS': {
+            'Prop': {
+                'QosStatus': 'r-'
+            }
+        },
+        'Multicast': {
+            'stream': {
+                'Dynamic': {
+                    'Prop': {
+                        'ActiveStreams': 'r-'
+                        },
+                    'Cmd': (
+                        'ClearActiveStreams',
+                        )
+                    },
+                'Static': {
+                    'Prop': {
+                        'StaticStreams': 'r-'
+                    }
+                },
             },
-            'Cmd': (
-                'ResetGeneralCounters',
-            )
-        },
-        'ProtocolCounters': {
-            'IgmpCounters': {
-                'Prop': {
-                    'IgmpProtocolList': 'r-'
-                    },
-                'Cmd': (
-                    'ResetIgmpCounters',
-                    )
-                },
-            'DhcpCounters': {
-                'Prop': {
-                    'DhcpProtocolList': 'r-'
-                    },
-                'Cmd': (
-                    'ResetDhcpCounters',
-                    )
-                },
-            'ArpCounters': {
-                'Prop': {
-                    'ArpProtocolList': 'r-'
-                    },
-                'Cmd': (
-                    'ResetArpCounters',
-                    )
-                },
-            'PPPoECounters': {
-                'Prop': {
-                    'PPPoEProtocolList': 'r-'
-                    },
-                'Cmd': (
-                    'ResetPPPoECounters',
-                    )
-                },
-            'UnknownSourceMACCounters': {
-                'Prop': {
-                    'UnknownSrcMACProtocolList': 'r-'
-                },
-                'Cmd': (
-                    'ResetUnknownSrcMACCounters',
-                )
-            },
-        },
-    },
-    'TLSMacForwardingList': {
-        'Prop': {
-            'MacForwardingList': 'r-'
-        },
-        'Cmd': (
-            'FlushMacForwardingList',
-        )
-    },
-    '1to1MacForwardingList': {
-        'Prop': {
-            'One2OneMacForwardingList': 'r-'
-        }
-    },
-    'Qos': {
-        'Prop': {
-            'wfqueues': 'r-'
-        }
-    },
-    'Multicast': {
-        'stream': {
-            'Dynamic': {
-                'Prop': {
-                    'ActiveStreams': 'r-'
-                    },
-                'Cmd': (
-                    'ClearActiveStreams',
-                    )
-                },
-            'Static': {
-                'Prop': {
-                    'StaticStreams': 'r-'
-                }
-            },
-        },
 
-        'Vlan': {
-            'Prop': {
-                'AttachedVlans': 'r-'
+            'Vlan': {
+                'Prop': {
+                    'AttachedVlans': 'r-'
+                }
+
             }
-        },
-        'Preview': {
-            'Cmd': (
-                'ResetPreviewSettings',
-            )
-        },
-        'Bandwidth': {
-            'Prop': {
-                'bandwidthStatus': 'r-'
-            }
-        },
-    },
-    'LineTest': {
-        'MELT': {
-            'Prop': {
-                'MeltResults': 'r-'
-            },
-            'Cmd': (
-                'StartMeltMeasurement',
-            )
-        },
-        'Delt': {
-            'Prop': {
-                'DeltMeasurementStatus': 'r-',
-                'RecordedDeltMeasurements': 'r-'
-            },
-            'Cmd': (
-                'StartDeltMeasurement',
-            )
-        },
-        'Selt': {
-            'Prop': {
-                'SeltMeasurementStatus': 'r-',
-                'RecordedSeltMeasurements': 'r-',
-                'CableType': 'rw',
-                'BandplanProfile': 'rw',
-                'TargetSnrm': 'rw'
-            },
-            'Cmd': (
-                'StartSeltMeasurement',
-            )
-        },
-    },
-    'Defects': {
-        'Prop': {
-            'Defects': 'r-'
-        }
-    },
-    'LineInventory': {
-        'Prop': {
-            'VendorId': 'r-'
-        }
-    },
-    'Maintenance': {
-        'Prop': {
-            'DslOperationStatus': 'r-'
-        }
-    },
-    'Subcarrier': {
-        'Cmd': (
-            'ShowBitAllocation',
-        )
-    },
-    'RfiBands': {
-        'Prop': {
-            'NotchStatus': 'r-'
         }
     }
-}
